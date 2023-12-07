fad80f0305d09a2bed20c58226cf589937074c20
Ken L'Héritier <ken.lheritier@gmail.com>
2023-12-07 09:44:56 +0100

    Update design

--- Sample App SwiftUI/DidomiButtonStyle.swift
+++ Sample App SwiftUI/DidomiButtonStyle.swift
@@ -11,9 +11,10 @@ struct DidomiButtonStyle: ButtonStyle {
         configuration.label
             .frame(width: 200)
             .padding(EdgeInsets(top: 21, leading: 14, bottom: 21, trailing: 14))
-            .background(Color("blue-5").opacity(configuration.isPressed ? 0.9 : 1))
+            .background(Color(.electricBlue50).opacity(configuration.isPressed ? 0.9 : 1))
             .foregroundColor(.white)
-            .font(.custom("IBM Plex Sans Bold", fixedSize: 14.0))
+            .font(.custom("Montserrat", fixedSize: 14.0))
+            .fontWeight(.medium)
             .cornerRadius(/*@START_MENU_TOKEN@*/4.0/*@END_MENU_TOKEN@*/)
     }
 }
